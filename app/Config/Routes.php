b1551711e7df016d825d605e4671121acc1bef33
iamNGT <guytresor1811@gmail.com>
2021-12-17 12:34:47 +0200

    finish auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/login','AuthController::index');
+$routes->post('/login', 'AuthController::login');
+$routes->post('/logout', 'AuthController::logout');
 $routes->get('time', function () {
     echo Factory::create()->sentence();
 });
--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,36 +3,47 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\User;
+use mysqli_sql_exception;
 
 class AuthController extends BaseController
 {
+
+    protected $db ;
+
+    public function __construct() {
+        $this->db = \Config\Database::connect();
+        helper(['url']);
+    }
+
     public function index()
     {
         helper(['form']);
-        echo view('login');
+        echo view('Auth/Login');
     }
 
     public function login(){
+        // $db = \Config\Database::connect();
         $session = session();
 
         $email = $this->request->getPost('email');
         $password = $this->request->getPost('password');
 
-        $query = $this->db->query("SELECT * FROM users WHERE users.email = $email");
-        $data = $query->getResult();
+        $query = $this->db->query('SELECT * FROM users WHERE email = '.$this->db->escape($email));
+        $data = $query->getRowArray();
         if ($data) {
             $pass = $data['password'];
-            $authenticatePassword = password_verify($password, $pass);
+            $authenticatePassword = $password === $pass ? $pass : null;
             if ($authenticatePassword) {
                 $session_data = [
                     'id' => $data['id'],
-                    'name' => $data['name'],
+                    'name' => $data['fullName'],
                     'email' => $data['email'],
                     'isLoggedIn' => TRUE
                 ];
 
                 $session->set($session_data);
-                return redirect()->to('/profile');
+                return redirect()->to(base_url());
             } else {
                 $session->setFlashdata('msg', 'Email or Password is incorrect.');
                 return redirect()->to('/login');
@@ -48,7 +59,13 @@ class AuthController extends BaseController
         $session = session();
 
         if($session->get('isLoggedIn')) {
-            $session->set(['isLoggedIn' => FALSE]);
+            $session->set([
+                'id' => '',
+                'name' => '',
+                'email' => '',
+                'isLoggedIn' => FALSE
+            ]);
+            return redirect()->to(base_url());
         }
 
     }
